32337505e65a94b77c1132cbd701653209a251c1
Mike Smith <mcsmith1989@gmail.com>
2022-05-25 10:46:57 -0400

    Including light updates and remove data from errors

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ def creatNewPlayer():
     None
     #Add input output logic here then create the player
 
-
-
 #Just testing it worked 
 mike = Player("Mike", "Smith", "test@testemail.com")
+mike.addWeapon("Longsword")
+mike.addItem("Frogs Tears")
+mike.updateBalance("gold", 500)
 
 print(vars(mike))
--- player.py
+++ player.py
@@ -1,18 +1,20 @@
 #create the player and associated details 
-from graphql import SchemaMetaFieldDef
-
 
 class Player():
     def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.balance = float(0) 
+        self.balance = {"gold":float(0), "silver":float(0)}
         self.weapons = {}
+        self.items = {}
 
-    def updateBalance(self, amount): 
-        self.balance += (amount)
+    def updateBalance(self, type, amount): 
+        self.balance[type] += (amount)
         print(f"{self.first_name}'s balance is now {self.balance}")
 
     def addWeapon(self, weapon):
         self.weapons.update({weapon:{}})
+    
+    def addItem(self, item):
+        self.items.update({item:{}})
